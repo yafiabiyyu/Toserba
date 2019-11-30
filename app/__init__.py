a3ed49399900c191fc7688142ce0b7bf8b66563e
yafiabiyyu <luckyveri3@gmail.com>
2019-11-30 14:53:16 +0700

    add models

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import app_config
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    @app.route('/')
-    def test():
-        return 'Hello'
+    login_manager.init_app(app)
+    login_manager.login_message = "Kamu harus login terlebih dahulu"
+    login_manager.login_view = 'auth.login'
+    migrate = Migrate(app,db)
+    
+    from app import models
 
     return app
