ae51b30c6e422eaa200a70bcf26350eb80623df8
yafiabiyyu <luckyveri3@gmail.com>
2019-12-03 01:29:04 +0700

    hapus asset

--- app/auth/views.py
+++ app/auth/views.py
@@ -8,7 +8,7 @@ def login():
         periksa_pengguna = Pengguna.query.filter_by(username=request.form['username']).first()
         if periksa_pengguna is not None and periksa_pengguna.verify_password(request.form['password']):
             login_user('periksa_pengguna')
-            return redirect(url_for('home.dashboard'))
+            return redirect(url_for('cashier.dashboard'))
         else:
             return redirect(url_for('auth.login'))
     return render_template('auth/index.html', title="Login")
